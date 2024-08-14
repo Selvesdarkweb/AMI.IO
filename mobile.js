f677b249f01cb62128761b01d2210d569b3d6955
Selvesdarkweb <109758678+Selvesdarkweb@users.noreply.github.com>
2024-08-14 20:06:36 +0530

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -18,31 +18,54 @@ class Paper {
   rotating = false;
 
   init(paper) {
-    paper.addEventListener('touchmove', (e) => {
+    const startHandler = (e) => {
+      if (this.holdingPaper) return;
+      this.holdingPaper = true;
+      
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+      
+      if (e.touches) { // For touch events
+        this.touchStartX = e.touches[0].clientX;
+        this.touchStartY = e.touches[0].clientY;
+      } else { // For mouse events
+        this.touchStartX = e.clientX;
+        this.touchStartY = e.clientY;
+      }
+      this.prevTouchX = this.touchStartX;
+      this.prevTouchY = this.touchStartY;
+    };
+
+    const moveHandler = (e) => {
       e.preventDefault();
-      if(!this.rotating) {
+      if (this.rotating) return;
+
+      if (e.touches) { // For touch events
         this.touchMoveX = e.touches[0].clientX;
         this.touchMoveY = e.touches[0].clientY;
-        
-        this.velX = this.touchMoveX - this.prevTouchX;
-        this.velY = this.touchMoveY - this.prevTouchY;
+      } else { // For mouse events
+        this.touchMoveX = e.clientX;
+        this.touchMoveY = e.clientY;
       }
-        
-      const dirX = e.touches[0].clientX - this.touchStartX;
-      const dirY = e.touches[0].clientY - this.touchStartY;
-      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
+
+      this.velX = this.touchMoveX - this.prevTouchX;
+      this.velY = this.touchMoveY - this.prevTouchY;
+
+      const dirX = this.touchMoveX - this.touchStartX;
+      const dirY = this.touchMoveY - this.touchStartY;
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
       const dirNormalizedX = dirX / dirLength;
       const dirNormalizedY = dirY / dirLength;
 
       const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
       let degrees = 180 * angle / Math.PI;
       degrees = (360 + Math.round(degrees)) % 360;
-      if(this.rotating) {
+      if (this.rotating) {
         this.rotation = degrees;
       }
 
-      if(this.holdingPaper) {
-        if(!this.rotating) {
+      if (this.holdingPaper) {
+        if (!this.rotating) {
           this.currentPaperX += this.velX;
           this.currentPaperY += this.velY;
         }
@@ -51,24 +74,21 @@ class Paper {
 
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
-    })
+    };
 
-    paper.addEventListener('touchstart', (e) => {
-      if(this.holdingPaper) return; 
-      this.holdingPaper = true;
-      
-      paper.style.zIndex = highestZ;
-      highestZ += 1;
-      
-      this.touchStartX = e.touches[0].clientX;
-      this.touchStartY = e.touches[0].clientY;
-      this.prevTouchX = this.touchStartX;
-      this.prevTouchY = this.touchStartY;
-    });
-    paper.addEventListener('touchend', () => {
+    const endHandler = () => {
       this.holdingPaper = false;
       this.rotating = false;
-    });
+    };
+
+    // Add event listeners for touch and mouse interactions
+    paper.addEventListener('touchstart', startHandler);
+    paper.addEventListener('touchmove', moveHandler);
+    paper.addEventListener('touchend', endHandler);
+
+    paper.addEventListener('mousedown', startHandler);
+    paper.addEventListener('mousemove', moveHandler);
+    paper.addEventListener('mouseup', endHandler);
 
     // For two-finger rotation on touch screens
     paper.addEventListener('gesturestart', (e) => {
